8028ba03912f0cf1db19b0070199706016620d9c
phietroaguilar <davibelzvieira@gmail.com>
2026-06-11 12:54:49 -0300

    finalizado

---
+++ app/src/main/java/com/davi/atividade1/ui/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.davi.atividade1.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import java.util.ArrayList
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity){
+
+    private val fragmentList: MutableList<Fragment> = ArrayList()
+    private val titleList: MutableList<Int> = ArrayList()
+
+    fun getTittle(position: Int): Int{
+        return titleList[position]
+    }
+    fun addFragment(fragment: Fragment, title: Int){
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+    override fun createFragment(position: Int): Fragment{
+        return fragmentList[position]
+    }
+    override fun getItemCount(): Int{
+        return fragmentList.size
+    }
+}
---
+++ app/src/main/java/com/davi/atividade1/ui/auth/DoneFragment.kt
@@ -0,0 +1,25 @@
+package com.davi.atividade1.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.davi.atividade1.databinding.DoneFragmentBinding
+
+class DoneFragment : Fragment() {
+
+    private var _binding: DoneFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = DoneFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
